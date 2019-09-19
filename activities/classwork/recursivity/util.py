39a50112a2e11b0ee7193a91e040fc36203f4d3e
PRECIROCK <precisadidas@hotmail.com>
2019-09-18 19:17:32 -0500

    Create recursive implementation

--- activities/classwork/recursivity/util.py
+++ activities/classwork/recursivity/util.py
@@ -1,5 +1,46 @@
 import functools
 import time
+import logging
+from util import TriangleBuilder, caching, timeit
+
+logger = logging.getLogger(__name__)
+
+
+
+def caching(func):
+    CACHE = {}
+
+    def wrapper(self, **kwargs):
+        key = hash(frozenset(kwargs.items())) ## generas un hash para generar un id unico porque ya tenia esos argumentos.
+        if key in CACHE:
+            return CACHE[key]
+        CACHE[key] = func(self, **kwargs)
+        return wrapper(self, **kwargs)
+
+    return wrapper
+
+
+def timeit(logger):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self, args, *kwargs):
+            start = time.time()
+            output = func(self, args, *kwargs)
+            logger.info("Execution time %s" % (time.time() - start))
+            return output
+
+        return wrapper
+
+    return decorator
+
+
+def _corner_case_decorator(func):
+    def wrapper(self, i, j, *args, **kwargs):
+        if j >= i or j == 0:
+            return 1
+        return func(self, i=i, j=j, *args, **kwargs)
+
+    return wrapper
 
 
 class TriangleBuilder(object):
@@ -19,3 +60,4 @@ class TriangleBuilder(object):
 
     def get_row(self, index):
         pass
+ exit()
