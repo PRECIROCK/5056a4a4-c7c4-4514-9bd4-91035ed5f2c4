10368c64d4f639ef658daec904e4b2a26d7c434d
PRECIROCK <precisadidas@hotmail.com>
2019-12-01 12:38:46 -0600

    ejer

--- activities/classwork/recursivity/main.py
+++ activities/classwork/recursivity/main.py
@@ -1,5 +1,6 @@
 import fire
 import logging
+
 from util import TriangleBuilder, caching, timeit
 
 logger = logging.getLogger(_name_)
@@ -8,10 +9,14 @@ logger = logging.getLogger(_name_)
 class Main(object):
     builder = TriangleBuilder()
 
+    @staticmethod
+    def message(content):
+        logging.info(content)
+
     @caching
     def get_element_recursively(self, i, j):
         return 1 if (j >= i or j == 0) else \
-            self.get_element_recursively(i=i - 1, j=j) + self.get_element_recursively(i=i - 1, j=j - 1)
+            self.get_element_recursively(i = i-1, j=j) + self.get_element_recursively(i = i-1, j=j-1)
 
     def pascal_triangle_a(self, level, index=0):
         if index < level:
@@ -21,9 +26,9 @@ class Main(object):
 
     def pascal_triangle_b(self, level, index):
         if index < level:
-            row = self.builder.get_row(index=index)
+            row = self.builder.get_row(index = index)
             print(" ".join(row))
-            self.pascaL_triangle_b(level, index=index + 1)
+            self.pascal_triangle_b(level,index=index+1)
 
     @timeit(logger)
     def pascal_triangle(self, level, option):
@@ -32,8 +37,7 @@ class Main(object):
         elif "b" == option.lower():
             self.pascal_triangle_b(level=level, index=0)
         else:
-            logging.info(f"Option {option} undefined.")
+            logger.info(f"Option {option} undefined.")
 
-
-if _name_ == "_main_":
-    fire.Fire(Main)
+    if _name_ == "_main_":
+        fire.Fire(Main)
--- activities/classwork/recursivity/util.py
+++ activities/classwork/recursivity/util.py
@@ -1,36 +1,28 @@
 import functools
 import time
-import logging
-from util import TriangleBuilder, caching, timeit
-
-logger = logging.getLogger(__name__)
-
 
 
 def caching(func):
     CACHE = {}
-
     def wrapper(self, **kwargs):
-        key = hash(frozenset(kwargs.items())) ## generas un hash para generar un id unico porque ya tenia esos argumentos.
+        key = hash(frozenset(kwargs.items()))
         if key in CACHE:
             return CACHE[key]
         CACHE[key] = func(self, **kwargs)
-        return wrapper(self, **kwargs)
-
+        return wrapper(self,**kwargs)
     return wrapper
-
 
 def timeit(logger):
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(self, args, *kwargs):
+        def wrapper(self,args,*kwargs):
             start = time.time()
-            output = func(self, args, *kwargs)
-            logger.info("Execution time %s" % (time.time() - start))
+            output = func(self,args,*kwargs)
+            end = time.time()
+            logger.info("Start time %s End time %s" % (start,end))
+            logger.info("Execution time %s" % (end - start))
             return output
-
         return wrapper
-
     return decorator
 
 
@@ -39,25 +31,27 @@ def _corner_case_decorator(func):
         if j >= i or j == 0:
             return 1
         return func(self, i=i, j=j, *args, **kwargs)
-
-    return wrapper
-
+    return wrapper()
 
 class TriangleBuilder(object):
     CACHE = {}
 
     def save(self, i, j, value):
-        pass
+        self.CACHE[(i,j)] = lambda: value
+        return value
 
-    def get(self, i, j, default=None):
-        pass
+    @_corner_case_decorator
+    def get(self, i, j, default=lambda: None):
+        return self.CACHE.get((i, j), default)()
 
+    @_corner_case_decorator
     def create(self, i, j):
-        pass
+        upper_left = self.get_or_create(i=i-1, j=i-1)
+        upper_center = self.get_or_create(i=i-1, j=j)
+        return self.save(i=i, j=j, value=upper_center+upper_left)
 
     def get_or_create(self, i, j):
-        pass
+        return self.get(i, j, default=lambda: self.create(i=i, j=j))
 
     def get_row(self, index):
-        pass
- exit()
+        return [str(self.get_or_create(index, j)) for j in range(index+1)]
